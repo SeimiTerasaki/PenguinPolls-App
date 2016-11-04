0b1838e9f41a94156772066a3bd6083f05212470
Seimi Terasaki <seimiterasaki@gmail.com>
2016-11-04 14:30:32 +0000

    second commit

--- app/auth/config.js
+++ app/auth/config.js
@@ -2,12 +2,12 @@ var ids = {
   github: {
     clientID: process.env.GITHUB_ID,
     clientSecret: process.env.GITHUB_KEY,
-    callbackURL: "https://fcc-votingapp-sterasaki.c9users.io/auth/github/callback"
+    callbackURL: "https://penguinpolls.herokuapp.com/auth/github/callback"
   },
   facebook: {
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: "https://fcc-votingapp-sterasaki.c9users.io/auth/facebook/callback"
+    callbackURL: "https://penguinpolls.herokuapp.com/auth/facebook/callback"
   },
 };
 
